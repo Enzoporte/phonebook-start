4afb526be01593e35f94e05de1b2d5495221dc7a
Matyrela <mativaresc@gmail.com>
2022-10-12 10:52:11 -0300

    Main

--- src/Program/Program.cs
+++ src/Program/Program.cs
@@ -8,11 +8,12 @@ namespace Program
         static void Main(string[] args)
         {
             // Crear el contacto dueño
-
+            Contact dueño = new Contact("Pepito");
             // Crear la lista de contactos
-
+            Phonebook contactos = new Phonebook(dueño);
             // Agregar contactos a la lista
-
+            Contact Enzo = new Contact("Enzo porte");
+            contactos.AddContact(Enzo);
             // Enviar un correo a algunos contactos
 
             // Enviar un WhatsApp a algunos contactos
